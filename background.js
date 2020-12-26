4d150c66a6383f403c44a10ea8afe5694124f8f9
Shreyas Mathur <shreyasmk.mathur@gmail.com>
2020-12-26 18:37:27 +0530

    Create background.js

---
+++ background.js
@@ -0,0 +1,13 @@
+notifOptionsStart = {
+    type: "basic",
+    iconUrl: "icon.png",
+    title: "Notes bot is active",
+    message: "The notes bot is now active and will be taking your notes",
+    contextMessage: "Use stop button to end the recording and email you the notes"
+}
+notifOptionsStop = {
+    type: "basic",
+    iconUrl: "icon.png",
+    title: "Your notes have been sent over email",
+    message: "Notes have been successfully sent over to your email"
+}
