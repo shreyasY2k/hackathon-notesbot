1b32ffc6369ba5cd978b6005b8ebcc29d3967cf9
Shreyas Mathur <shreyasmk.mathur@gmail.com>
2020-12-26 15:35:19 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,1 +1,20 @@
+var summaryBot= {};
+document.querySelector('.update').addEventListener("click",()=>{
+    var email =document.querySelector('.betaEmail').value;
+    console.log(email);
+})
 
+if(email!=""){
+    document.querySelector('.first').style.display='none';
+    document.querySelector('.success').style.display='block';
+
+    summaryBot["email"]= email;
+
+    chrome.storage.sync.set({
+        summaryBot
+    },function(){
+        console.log(summaryBot);
+
+    })
+
+}
