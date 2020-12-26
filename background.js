1205b3a34123bb7e521dd45febe68321bcf6d410
Shreyas Mathur <shreyasmk.mathur@gmail.com>
2020-12-26 18:42:38 +0530

    Update background.js

--- background.js
+++ background.js
@@ -11,9 +11,9 @@ notifOptionsStop = {
     title: "Your notes have been sent over email",
     message: "Notes have been successfully sent over to your email"
 }
-chrome.storage.sync.get(['nithinSummaryBot'], function(result){
-    if(result && result['nithinSummaryBot']){
-        betaEmail = result['nithinSummaryBot']['email'];
+chrome.storage.sync.get(['summaryBot'], function(result){
+    if(result && result['summaryBot']){
+        betaEmail = result['summaryBot']['email'];
         notifOptionsStop = {
             type: "basic",
             iconUrl: "icon.png",
