68ab8b85aecbf17f3219670132487bf7c1bae15e
Shreyas Mathur <shreyasmk.mathur@gmail.com>
2020-12-26 15:50:30 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -2,19 +2,29 @@ var summaryBot= {};
 document.querySelector('.update').addEventListener("click",()=>{
     var email =document.querySelector('.betaEmail').value;
     console.log(email);
+    
+    
+    if(email!=""){
+        document.querySelector('.first').style.display='none';
+        document.querySelector('.success').style.display='block';
+    
+        summaryBot["email"]=email;
+    
+        chrome.storage.sync.set({
+            summaryBot
+        }, function() {
+            console.log(summaryBot);
+        });
+    }
+    
+    var data = new FormData();
+    data.append("email", email);
+    
+    var xhr = new XMLHttpRequest();
+    
+    xhr.addEventListener("readystatechange", function() {
+    if(this.readyState === 4) {
+        console.log(this.responseText);
+    }
+    });
 })
-
-if(email=""){
-    document.querySelector('.first').style.display='none';
-    document.querySelector('.success').style.display='block';
-
-    summaryBot["email"]= email;
-
-    chrome.storage.sync.set({
-        summaryBot
-    },function(){
-        console.log(summaryBot);
-
-    })
-
-}
